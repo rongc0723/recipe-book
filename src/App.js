956b2334977c4dddb6d75ab883726244a0a31edc
Rong Chen <rongc0723@gmail.com>
2023-03-22 11:53:16 -0400

    routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+//pages
+import Home from './pages/home/Home';
+import Recipe from './pages/recipe/Recipe';
+import Search from './pages/search/Search';
+import Create from './pages/create/Create';
+
 import './App.css'
+
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipe/:id" element={<Recipe />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/create" element={<Create />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/recipe/Recipe.js
@@ -0,0 +1,8 @@
+import './Recipe.css';
+import React from 'react'
+
+export default function Recipe() {
+  return (
+    <div>Recipe</div>
+  )
+}
