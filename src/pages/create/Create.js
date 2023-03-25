5570b93818b4ac500cb4201addf8ed10f72947cf
Rong Chen <rongc0723@gmail.com>
2023-03-25 19:27:00 -0400

    redirect user after new recipe

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,6 +1,7 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { useFetch } from '../../hooks/useFetch'
 import './Create.css'
+import { useNavigate } from 'react-router-dom'
 
 
 export default function Create() {
@@ -11,6 +12,7 @@ export default function Create() {
   const [newIngredient, setNewIngredient] = useState('')
   const [ingredients, setIngredients] = useState([])
   const ingredientInput = useRef(null)
+  const navigate = useNavigate()
   
   const {postData, data, error} = useFetch('http://localhost:3000/recipes', 'POST') 
 
@@ -28,6 +30,12 @@ export default function Create() {
     setNewIngredient('')
     ingredientInput.current.focus()
   }
+
+  useEffect( () => {
+    if (data) {
+      navigate('/')
+    }
+  }, [data, navigate])
 
 
   return (
